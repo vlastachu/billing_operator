7eb0d0c95e76b6584064c06e29fdf46be986c99a
vlad chuprin <vlastachu@gmail.com>
2017-03-17 04:45:29 +0300

    add lombok

--- core/exchange-web-app/src/main/java/model/Tariff.java
+++ core/exchange-web-app/src/main/java/model/Tariff.java
@@ -1,18 +1,12 @@
 package model;
 
+import lombok.Data;
 import model.entities.Call;
 
+@Data
 public class Tariff {
     private final int moneyPerMinute;
     private final static Tariff defaultLocalTariff = new Tariff(10);
-
-    public Tariff(int moneyPerMinute) {
-        this.moneyPerMinute = moneyPerMinute;
-    }
-
-    public int getMoneyPerMinute() {
-        return moneyPerMinute;
-    }
 
     public static Tariff getDefaultLocalTariff() {
         return defaultLocalTariff;
@@ -20,27 +14,5 @@ public class Tariff {
 
     public int getCallCost(Call call) {
         return call.getDuration() * moneyPerMinute;
-    }
-
-    @Override
-    public String toString() {
-        return "Tariff{" +
-                "moneyPerMinute=" + moneyPerMinute +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Tariff tariff = (Tariff) o;
-
-        return moneyPerMinute == tariff.moneyPerMinute;
-    }
-
-    @Override
-    public int hashCode() {
-        return moneyPerMinute;
     }
 }
--- core/exchange-web-app/src/main/java/model/entities/Call.java
+++ core/exchange-web-app/src/main/java/model/entities/Call.java
@@ -1,7 +1,10 @@
 package model.entities;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 @Cacheable(false)
 @Table(name = "CALLS")
@@ -24,35 +27,6 @@ public class Call {
     public Call() {
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Call call = (Call) o;
-
-        if (id != call.id) return false;
-        if (duration != call.duration) return false;
-        return phoneNumber != null ? phoneNumber.equals(call.phoneNumber) : call.phoneNumber == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (phoneNumber != null ? phoneNumber.hashCode() : 0);
-        result = 31 * result + duration;
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Call{" +
-                "id=" + id +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", duration=" + duration +
-                '}';
-    }
-
     public Call(int id, String phoneNumber, int duration) {
         this.id = id;
         this.phoneNumber = phoneNumber;
@@ -61,31 +35,6 @@ public class Call {
 
     public Call(String phoneNumber, int duration) {
         this.phoneNumber = phoneNumber;
-        this.duration = duration;
-    }
-
-    public int getId() {
-
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
         this.duration = duration;
     }
 }
